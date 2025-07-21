54d47cbfaf37e42c275fa265f801660d2fe6154f
Himars142 <127928240+Himars142@users.noreply.github.com>
2025-07-21 14:59:27 +0300

    Deleted useless import in category controller

--- src/main/java/com/example/demo3/controller/CategoryController.java
+++ src/main/java/com/example/demo3/controller/CategoryController.java
@@ -7,7 +7,6 @@ import jakarta.validation.constraints.Max;
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.PositiveOrZero;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
