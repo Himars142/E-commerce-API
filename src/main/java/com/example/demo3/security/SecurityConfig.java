dcc83ee384606b1071c831f2d2d40193447261fc
Himars142 <127928240+Himars142@users.noreply.github.com>
2025-07-22 20:56:54 +0300

    Allow access to get category by id endpoint in SecurityConfig

--- src/main/java/com/example/demo3/security/SecurityConfig.java
+++ src/main/java/com/example/demo3/security/SecurityConfig.java
@@ -27,7 +27,7 @@ public class SecurityConfig {
                                 "/api/products",
                                 "/api/products/**",
                                 "/api/categories",
-                                "/api/categories/{categoryId}/products").permitAll()
+                                "api/categories/{id}").permitAll()
                         .anyRequest().authenticated()
                 )
                 .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class);
